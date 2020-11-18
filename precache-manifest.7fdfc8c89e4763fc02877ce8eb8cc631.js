3552bae18140938827a6036e0004144679bf4a9f
newbeeguy <mskjavascript@gmail.com>
2020-11-18 21:09:37 +0300

    Updates

---
+++ precache-manifest.7fdfc8c89e4763fc02877ce8eb8cc631.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa1aa5485cd73f04c8011ab6ca819253",
+    "url": "/snab/index.html"
+  },
+  {
+    "revision": "ff8840b5c05f2eb444ba",
+    "url": "/snab/static/css/main.efe2dd11.chunk.css"
+  },
+  {
+    "revision": "d57412d12f86a2fbbd5e",
+    "url": "/snab/static/js/2.03626b6e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/snab/static/js/2.03626b6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff8840b5c05f2eb444ba",
+    "url": "/snab/static/js/main.f481a6d9.chunk.js"
+  },
+  {
+    "revision": "82916aa248e754b6e96e",
+    "url": "/snab/static/js/runtime-main.7bf5ba73.js"
+  },
+  {
+    "revision": "c364b20295f0508c392a4f6aa45e03d8",
+    "url": "/snab/static/media/k1.c364b202.jpg"
+  },
+  {
+    "revision": "4eb929ba38a001d77462b97557e98e78",
+    "url": "/snab/static/media/k2.4eb929ba.jpg"
+  },
+  {
+    "revision": "f9473484fe2e65d53b356b368639337c",
+    "url": "/snab/static/media/k3.f9473484.jpg"
+  },
+  {
+    "revision": "ea3a9956de599bc3be503c40f0bd20fe",
+    "url": "/snab/static/media/k4.ea3a9956.jpg"
+  },
+  {
+    "revision": "2058a822f50bf9d2109efcb36b5437ec",
+    "url": "/snab/static/media/k5.2058a822.jpg"
+  }
+]);
